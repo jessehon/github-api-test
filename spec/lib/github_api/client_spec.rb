984d6900385d0c0fe5604a6bc70af67a4f104f33
Jesse Hon <jesse@jessehon.com>
2019-05-15 16:07:14 +0800

    Create required describe and context blocks for client unit test

---
+++ spec/lib/github_api/client_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe GithubApi::Client do
+  let(:client) { GithubApi::Client.new }
+
+  describe '#fetch_user_organizations' do
+    context 'when valid' do
+    end
+
+    context 'when github responds 404'
+    end
+  end
+end
